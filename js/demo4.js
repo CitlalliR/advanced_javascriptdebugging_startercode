ad3d560fca6feb2eed7bd1f6d4a21cd998e49918
Citlalli <citlalli.estrella3@gmail.com>
2017-11-09 00:42:17 +0000

    m

--- js/demo4.js
+++ js/demo4.js
@@ -3,10 +3,18 @@ function magicianMagic() {
     return Math.floor((Math.random() * 100) + 1);
 }
 var secretNumber = magicianMagic();
+debugger;
 var input = prompt("Im thinking of a number between 1 - 100: ");
 
 if(secretNumber == input) {
     alert("Impressive!");
-} else {
+}
+    else if (secretNumber > input){
+        alert("think bigger");
+    }
+    else if (secretNumber < input){
+        alert("think smaller");
+    }
+ else {
 	alert("Wrong!");
 }
